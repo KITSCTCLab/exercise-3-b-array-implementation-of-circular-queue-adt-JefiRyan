012cda76cbf668d3ebc91a5e76a280ed9279ff37
JefiRyan <109643550+JefiRyan@users.noreply.github.com>
2022-08-10 11:20:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,29 +1,50 @@
 class MyCircularQueue:
     def __init__(self, size: int):
-        # Write code here
+        self.queue = [None]*size
+        self.size = size
+        self.front = -1
+        self.rear = -1
+        self.gap = 0
 
     def enqueue(self, value: int) -> bool:
-        # Write code here
+        if self.gap > 0:
+            self.rear = 0
+        if not self.is_full():
+            self.rear += 1
+            self.queue[self.rear] = value
+            return True
+        return False
 
     def dequeue(self) -> bool:
-        # Write code here
+        if not self.is_empty():
+            self.front += 1
+            self.queue[self.front] = None
+            self.gap += 1
+            return True
+        return False
 
     def get_front(self) -> int:
-        # Write code here
+        if not self.is_empty():
+            return self.queue[self.front]
+        return -1
 
     def get_rear(self):
-        # Write code here
+        if not self.is_empty():
+            return self.queue[self.rear]
+        return -1
 
     def is_empty(self):
-        # Write code here
+        return self.rear == -1
 
     def is_full(self):
-        # Write code here
+        return self.rear == self.size - 1
 
 
 # Do not change the following code
 operations = []
 for specific_operation in input().split(','):
+    specific_operation = specific_operation.strip("[")
+    specific_operation = specific_operation.strip("]")
     operations.append(specific_operation.strip())
 data = []
 for item in input().split(','):
@@ -31,23 +52,29 @@ for item in input().split(','):
     if item == '-':
         data.append([])
     else:
+        item = item.strip("[")
+        item = item.strip("]")
+        if item == "":
+            item = 0
         data.append([int(item)])
 obj = MyCircularQueue(data[0][0])
 result = []
+print(data)
+print(operations)
 for i in range(len(operations)):
     if i == 0:
         result.append(None)
-    elif operations[i] == "enqueue":
+    elif operations[i] == "'enqueue'":
         result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
+    elif operations[i] == "'get_rear'":
         result.append(obj.get_rear())
-    elif operations[i] == "get_front":
+    elif operations[i] == "'get_front'":
         result.append(obj.get_front())
-    elif operations[i] == "dequeue":
+    elif operations[i] == "'dequeue'":
         result.append(obj.dequeue())
-    elif operations[i] == "is_full":
+    elif operations[i] == "'is_full'":
         result.append(obj.is_full())
-    elif operations[i] == "is_empty":
+    elif operations[i] == "'is_empty'":
         result.append(obj.is_empty())
 
 print(result)
